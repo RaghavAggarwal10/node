5b3ef002fdc1afac83f10dfdaf431c53cdb9756b
RaghavAggarwal17 <aggarwalraghav702@gmail.com>
2023-11-04 09:05:32 +0530

    first commit

---
+++ modules.js
@@ -0,0 +1,8 @@
+const name=require('./names.js');
+const hi=require('./utils.js');
+const data=require('./alternatesyntax.js')
+require('./mind_grenade.js');
+hi('susan')
+hi(name.john)               
+hi(name.peter)
+// we see that it would be better if the names were in seperate files or functions
